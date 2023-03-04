cd343b0eb986fa9bc4f433c8536f3f1935fc0e86
Trevor Bonomi <trevorbonomi@gmail.com>
2023-03-04 18:24:49 +0100

    bubble-text needs work

--- src/Bubble.js
+++ src/Bubble.js
@@ -10,113 +10,11 @@ class Bubble extends Phaser.GameObjects.Container {
 
     this.headY = headY;
     this.message = message;
-    this.bWidth = 0;
-    this.bHeight = 0;
     this.maxWidth = 128;
 
     this.bubbleText = new Phaser.GameObjects.BitmapText(this.scene, 8, 6, "nin-dark", this.message);
     this.wrapBitmapText(this.bubbleText, this.maxWidth);
 
-    if (this.bubbleText.width > this.bWidth) {
-      this.bWidth = this.bubbleText.width + 14;
-    }
-    if (this.bubbleText.height > this.bHeight) {
-      this.bHeight = this.bubbleText.height + 10;
-    }
-    this.borders = [];
-    this.d = [
-      {
-        x: 4,
-        y: 4,
-        width: this.bWidth - 4,
-        height: this.bHeight - 4,
-        texture: "misc-bubble",
-        frame: 4,
-      },
-      { x: 0, y: 0, width: 4, height: 4, texture: "misc-bubble", frame: 0 },
-      {
-        x: 0 + this.bWidth,
-        y: 0,
-        width: 4,
-        height: 4,
-        texture: "misc-bubble",
-        frame: 2,
-      },
-      {
-        x: 0 + this.bWidth,
-        y: 0 + this.bHeight,
-        width: 4,
-        height: 4,
-        texture: "misc-bubble",
-        frame: 8,
-      },
-      {
-        x: 0,
-        y: 0 + this.bHeight,
-        width: 4,
-        height: 4,
-        texture: "misc-bubble",
-        frame: 6,
-      },
-      {
-        x: 4,
-        y: 0,
-        width: this.bWidth - 4,
-        height: 4,
-        texture: "misc-bubble",
-        frame: 1,
-      },
-      {
-        x: 4,
-        y: 0 + this.bHeight,
-        width: this.bWidth - 4,
-        height: 4,
-        texture: "misc-bubble",
-        frame: 7,
-      },
-      {
-        x: 0,
-        y: 4,
-        width: 4,
-        height: this.bHeight - 4,
-        texture: "misc-bubble",
-        frame: 3,
-      },
-      {
-        x: 0 + this.bWidth,
-        y: 4,
-        width: 4,
-        height: this.bHeight - 4,
-        texture: "misc-bubble",
-        frame: 5,
-      },
-      {
-        x: 0 + Math.floor(this.bWidth / 2) - 2,
-        y: this.bHeight,
-        width: 9,
-        height: 6,
-        texture: "misc-bubble-tail",
-        frame: null,
-      },
-    ];
-    // Create all of our corners and edges
-    for (var i = 0; i < this.d.length; i++) {
-      this.borders[i] = new Phaser.GameObjects.TileSprite(
-        this.scene,
-        this.d[i].x,
-        this.d[i].y,
-        this.d[i].width,
-        this.d[i].height,
-        this.d[i].texture,
-        this.d[i].frame
-      );
-    }
-
-    // Add all of the above to this sprite
-    for (var i = 0; i < this.borders.length; i++) {
-      this.borders[i].setOrigin(0, 0);
-      this.add(this.borders[i]);
-    }
     this.add(this.bubbleText);
     this.setMessage(this.message);
   }
@@ -128,101 +26,8 @@ class Bubble extends Phaser.GameObjects.Container {
       this.setVisible(true);
     }
     this.message = message;
-    this.bWidth = 0;
-    this.bHeight = 0;
-
     this.bubbleText.setText(this.message);
     this.wrapBitmapText(this.bubbleText, this.maxWidth);
-
-    if (this.bubbleText.width > this.bWidth) {
-      this.bWidth = this.bubbleText.width + 14;
-    }
-    if (this.bubbleText.height > this.bHeight) {
-      this.bHeight = this.bubbleText.height + 10;
-    }
-
-    this.d = [
-      {
-        x: 4,
-        y: 4,
-        width: this.bWidth - 4,
-        height: this.bHeight - 4,
-        texture: "misc-bubble",
-        frame: 4,
-      },
-      { x: 0, y: 0, width: 4, height: 4, texture: "misc-bubble", frame: 0 },
-      {
-        x: 0 + this.bWidth,
-        y: 0,
-        width: 4,
-        height: 4,
-        texture: "misc-bubble",
-        frame: 2,
-      },
-      {
-        x: 0 + this.bWidth,
-        y: 0 + this.bHeight,
-        width: 4,
-        height: 4,
-        texture: "misc-bubble",
-        frame: 8,
-      },
-      {
-        x: 0,
-        y: 0 + this.bHeight,
-        width: 4,
-        height: 4,
-        texture: "misc-bubble",
-        frame: 6,
-      },
-      {
-        x: 4,
-        y: 0,
-        width: this.bWidth - 4,
-        height: 4,
-        texture: "misc-bubble",
-        frame: 1,
-      },
-      {
-        x: 4,
-        y: 0 + this.bHeight,
-        width: this.bWidth - 4,
-        height: 4,
-        texture: "misc-bubble",
-        frame: 7,
-      },
-      {
-        x: 0,
-        y: 4,
-        width: 4,
-        height: this.bHeight - 4,
-        texture: "misc-bubble",
-        frame: 3,
-      },
-      {
-        x: 0 + this.bWidth,
-        y: 4,
-        width: 4,
-        height: this.bHeight - 4,
-        texture: "misc-bubble",
-        frame: 5,
-      },
-      {
-        x: 0 + Math.floor(this.bWidth / 2) - 2,
-        y: this.bHeight,
-        width: 9,
-        height: 6,
-        texture: "misc-bubble-tail",
-        frame: null,
-      },
-    ];
-
-    for (var i = 0; i < this.d.length; i++) {
-      this.borders[i].x = this.d[i].x;
-      this.borders[i].y = this.d[i].y;
-      this.borders[i].width = this.d[i].width;
-      this.borders[i].height = this.d[i].height;
-    }
 
     this.center();
   }
@@ -259,8 +64,8 @@ class Bubble extends Phaser.GameObjects.Container {
     });
   }
   center() {
-    this.x = -Math.floor(this.bWidth / 2) - 3;
-    this.y = -this.bHeight + this.headY;
+    this.x = -Math.floor(this.bubbleText.width / 2) - 5;
+    this.y = -this.bubbleText.height + this.headY;
   }
   update() {}
   destroy() {
