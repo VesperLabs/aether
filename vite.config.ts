131532737e5e791e50e2b7b7051d4e0476abf155
Trevor Bonomi <trevorbonomi@gmail.com>
2023-04-01 13:41:50 +0200

    fuck

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 require("dotenv").config();
 import react from "@vitejs/plugin-react-swc";
 
+console.log(`🛠 DEBUG: ${process.env.DEBUG}`);
+console.log(`🛠 SERVER_FPS: ${process.env.SERVER_FPS}`);
+console.log(`🛠 SERVER_URL: ${process.env.SERVER_URL}`);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
