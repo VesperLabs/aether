6f831d0d8839c4b8d116ef076f6b93396e59df40
Trevor Bonomi <trevorbonomi@gmail.com>
2023-07-17 20:47:36 +0200

    shared item scaling code

--- shared/index.ts
+++ shared/index.ts
@@ -1,7 +1,6 @@
 export { default as assetList } from "./data/assetList.json";
 export { default as buffList } from "./data/buffList.json";
 export { default as hairTints } from "./data/hairTints.json";
-export { default as itemList } from "./data/itemList.json";
 export { default as itemModsList } from "./data/itemModsList.json";
 export { default as itemSetList } from "./data/itemSetList.json";
 export { default as keepers } from "./data/keepers.json";
@@ -12,7 +11,7 @@ export { default as questList } from "./data/questList.json";
 export { default as skinTints } from "./data/skinTints.json";
 export { default as soundList } from "./data/soundList.json";
 export { default as spellDetails } from "./data/spellDetails.json";
-export { default as ItemBuilder, getItemCost } from "./ItemBuilder";
+export { default as ItemBuilder, getItemCost, itemList } from "./ItemBuilder";
 export * from "./Assets";
 export * from "./Character";
 export * from "./Door";
