73e5863bc57882302375b357adcf17190a159ec8
Trevor Bonomi <trevorbonomi@gmail.com>
2023-07-30 00:03:30 +0200

    assets proxy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,9 +7,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    // proxy: {
-    //   "/assets": { target: `http://127.0.0.1:${process.env.PORT}` },
-    // },
   },
   define: {
     "process.env.SERVER_URL": JSON.stringify(process.env.SERVER_URL),
