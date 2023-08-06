c15d08520e49d668f7f003f9a2f52e56e6af677f
Trevor Bonomi <trevorbonomi@gmail.com>
2023-08-06 14:41:00 +0200

    fixes

--- web/PageHome.tsx
+++ web/PageHome.tsx
@@ -9,13 +9,13 @@ export default function () {
         Welcome to Aether MMO. The game is under active development, but feel free to look around.
       </Text>
       <Flex sx={{ gap: 2, flexWrap: "wrap" }}>
-        <a href="http://aether.up.railway.app">
+        <a href="http://aether.up.railway.app" target="_blank">
           <Button variant="wood" sx={{ px: 4, py: 2 }}>
             Play Now
           </Button>
         </a>
-        <a href="https://discord.gg/yadDyqpb6D">
-          <Button as={Link} variant="wood" sx={{ px: 4, py: 2 }}>
+        <a href="https://discord.gg/yadDyqpb6D" target="_blank">
+          <Button variant="wood" sx={{ px: 4, py: 2 }}>
             Join Discord
           </Button>
         </a>
