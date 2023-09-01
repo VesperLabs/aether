3c8ec6a7026487880e6697597b43ea9405c912e1
Trevor Bonomi <trevorbonomi@gmail.com>
2023-09-01 23:59:39 +0200

    arf

--- build.server.js
+++ build.server.js
@@ -6,7 +6,7 @@ build({
   entryPoints: ["server/main.ts"],
   bundle: true,
   minify: true,
-  external: [...Object.keys(dependencies), ...Object.keys(peerDependencies || [])],
+  external: Object.keys(dependencies).concat(Object.keys(peerDependencies || [])),
   platform: "node", // for CJS
   outfile: "dist/server.js",
   define: {
@@ -14,6 +14,6 @@ build({
     "process.env.SERVER_FPS": JSON.stringify(process.env.SERVER_FPS),
     "process.env.MONGO_URL": JSON.stringify(process.env.MONGO_URL),
     "process.env.SERVER_URL": JSON.stringify(process.env.SERVER_URL),
-    "process.env.PORT": JSON.stringify(process.env.PORT ?? 3000),
+    "process.env.PORT": JSON.stringify(process.env.PORT),
   },
 });
