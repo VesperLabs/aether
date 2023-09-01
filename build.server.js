6ff6f1a8d25da73ec28559c21454d429a9541442
Trevor Bonomi <trevorbonomi@gmail.com>
2023-09-02 00:07:57 +0200

    builder

--- build.server.js
+++ build.server.js
@@ -1,6 +1,5 @@
 const { build } = require("esbuild");
 const { dependencies, peerDependencies } = require("./package.json");
-require("dotenv").config();
 
 build({
   entryPoints: ["server/main.ts"],
@@ -9,11 +8,4 @@ build({
   external: Object.keys(dependencies).concat(Object.keys(peerDependencies || [])),
   platform: "node", // for CJS
   outfile: "dist/server.js",
-  define: {
-    "process.env.PUBLIC_DIR": JSON.stringify(process.env.PUBLIC_DIR),
-    "process.env.SERVER_FPS": JSON.stringify(process.env.SERVER_FPS),
-    "process.env.MONGO_URL": JSON.stringify(process.env.MONGO_URL),
-    "process.env.SERVER_URL": JSON.stringify(process.env.SERVER_URL),
-    "process.env.PORT": JSON.stringify(process.env.PORT ?? 3000),
-  },
 });
