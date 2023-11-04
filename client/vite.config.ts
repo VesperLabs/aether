ddce86137e32fcb039d13448d2226378b32a8563
Trevor Bonomi <trevorbonomi@gmail.com>
2023-11-04 13:00:27 +0100

    fix ports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,8 +11,8 @@ export default ({ mode }) => {
   console.log(`🛠 SERVER_FPS: ${process.env.SERVER_FPS}`);
   console.log(`🛠 SERVER_URL: ${process.env.SERVER_URL}`);
   console.log(`🛠 ASSETS_URL: ${process.env.ASSETS_URL}`);
-  console.log(`🛠 PEER_SERVER_PORT: ${process.env.PEER_SERVER_PORT}`);
   console.log(`🛠 PEER_CLIENT_PORT: ${process.env.PEER_CLIENT_PORT}`);
+  console.log(`🛠 PEER_CLIENT_PATH: ${process.env.PEER_CLIENT_PATH}`);
 
   return defineConfig({
     envDir: "../",
@@ -32,8 +32,8 @@ export default ({ mode }) => {
       "process.env.SERVER_FPS": process.env.SERVER_FPS,
       "process.env.SERVER_URL": JSON.stringify(process.env.SERVER_URL),
       "process.env.ASSETS_URL": JSON.stringify(process.env.ASSETS_URL),
-      "process.env.PEER_SERVER_PORT": JSON.stringify(process.env.PEER_SERVER_PORT),
       "process.env.PEER_CLIENT_PORT": JSON.stringify(process.env.PEER_CLIENT_PORT),
+      "process.env.PEER_CLIENT_PATH": JSON.stringify(process.env.PEER_CLIENT_PATH),
     },
     server: {
       host: true,
