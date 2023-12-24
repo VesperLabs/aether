526e160647bd1a855737ae2f3e007bedf495c594
Trevor Bonomi <trevorbonomi@gmail.com>
2023-12-24 13:23:39 +0100

    hovering

--- client/game/SceneMain.js
+++ client/game/SceneMain.js
@@ -326,9 +326,9 @@ function getClosestEntity({ scene, hero, entities }) {
 
   const cursorPoint = scene.input.activePointer.positionToCamera(scene.cameras.main);
 
-  entities.forEach((entity) => {
+  for (const entity of entities) {
+    if (!entity.state) continue; // not initialized yet.
     updateHoverState(entity, cursorPoint);
-
     const distance = distanceTo(entity, hero);
     if (["sign", "keeper"].includes(entity?.kind) && distance < closestDistance) {
       closestEntity = entity;
@@ -336,13 +336,12 @@ function getClosestEntity({ scene, hero, entities }) {
     } else {
       entity.checkStealth({ distance });
     }
-  });
+  }
 
   return closestEntity;
 }
 
 function updateHoverState(entity, cursorPoint) {
-  if (!entity.state) return;
   const cursorDistance = distanceTo(entity, cursorPoint);
   const hoverDistance = (entity?.hitBoxSize?.width + entity?.hitBoxSize?.height) / 2;
   entity.state.isHovering = cursorDistance < hoverDistance;
@@ -352,6 +351,7 @@ function updateHoverState(entity, cursorPoint) {
 function getNearbyPlayers(hero, players) {
   let nearbyPlayers = [];
   for (const player of players) {
+    if (!player.state) continue; // not initialized yet.
     const distance = distanceTo(player, hero);
     // adjust player stealth
     player.checkStealth({ distance });
