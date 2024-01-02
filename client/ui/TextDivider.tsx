b417e6220e753fb4eedc3ff5d2a432d8624589df
Trevor Bonomi <trevorbonomi@gmail.com>
2024-01-02 18:50:26 +0100

    remove scroll for now

--- client/ui/TextDivider.tsx
+++ client/ui/TextDivider.tsx
@@ -1,10 +1,39 @@
-import { Box, Text, Divider } from "@aether/ui";
+import { Box, Divider, Text, theme } from "@aether/ui";
+import { get } from "lodash";
 
-const TextDivider = ({ children, sx, color = "gray.500" }: any) => (
-  <Box py={2} sx={{ width: "100%", textAlign: "center", ...sx }}>
-    <Divider sx={{ my: 0, mb: -2, mt: 2, zIndex: -1 }} />
-    <Text sx={{ pb: 2, mb: -1, color, position: "relative" }}>{children}</Text>
-  </Box>
-);
+// Utility function to convert hex color to RGBA
+const hexToRGBA = (hex, alpha = 1) => {
+  let r = parseInt(hex.slice(1, 3), 16),
+    g = parseInt(hex.slice(3, 5), 16),
+    b = parseInt(hex.slice(5, 7), 16);
+
+  if (alpha > 1) alpha = 1;
+  if (alpha < 0) alpha = 0;
+
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+};
+
+const TextDivider = ({ children, sx, color = "gray.500", alpha = 0.5 }: any) => {
+  const resolvedColor = get(theme, `colors.${color}`) ?? color;
+  const colorWithAlpha = hexToRGBA(resolvedColor, alpha);
+
+  return (
+    <Text
+      sx={{
+        py: 2,
+        color,
+        position: "relative",
+        display: "block",
+        width: "100%",
+        textAlign: "center",
+        backgroundImage: `linear-gradient(to bottom, transparent 50%, ${colorWithAlpha} 50%, ${colorWithAlpha} 52%, transparent 52%)`,
+        backgroundPosition: "center",
+        ...sx,
+      }}
+    >
+      {children}
+    </Text>
+  );
+};
 
 export default TextDivider;
